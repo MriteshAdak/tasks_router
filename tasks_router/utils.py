9a6e17bbee58e085da0321d58c6ea5fef982dca7
MriteshAdak <mriteshadak@outlook.com>
2026-05-11 20:56:00 +0200

    Add user conversion utility functions to convert UserModel to UserDTO schema

--- tasks_router/utils.py
+++ tasks_router/utils.py
@@ -2,7 +2,8 @@
 
 from tasks_router.models.task_model import Task as TaskModel
 from tasks_router.schema.task_response import TaskResponse
-
+from tasks_router.models.user_model import User as UserModel
+from tasks_router.schema.user_schema import User as UserDTO
 
 def convert_task_model_to_response(task: TaskModel) -> TaskResponse: # handle bad inputs
     """Convert TaskModel instance to TaskResponse schema."""
@@ -20,3 +21,12 @@ def convert_task_models_to_responses(tasks: list[TaskModel]) -> list[TaskRespons
     """Convert list of TaskModel instances to list of TaskResponse schemas."""
 
     return [convert_task_model_to_response(task) for task in tasks]
+
+
+def convert_user_model_to_user_dto_schema(user: UserModel) -> UserDTO:
+    """Convert UserModel instance to UserDTO schema."""
+
+    return UserDTO(
+        username=user.username,
+        display_name=user.display_name
+    )
