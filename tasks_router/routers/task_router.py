ee2c2eec5e94ddff42cab11c84d6ea028b6c34ff
MriteshAdak <mriteshadak@outlook.com>
2026-05-12 09:42:49 +0200

    Refactor task router to add type hints for task_services variable

--- tasks_router/routers/task_router.py
+++ tasks_router/routers/task_router.py
@@ -15,20 +15,20 @@ _db = Database(settings)
 
 @router.get("/", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
 def get_tasks(user_id: uuid.UUID, db: Session = Depends(_db.get_db)) -> list[TaskResponse]:
-    task_services = TaskServices(TaskRepository(db))
+    task_services: TaskServices = TaskServices(TaskRepository(db))
     return task_services.get_all(user_id)
 
 @router.post("/", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
 def create_task(task: TaskCreate, db: Session = Depends(_db.get_db)) -> TaskResponse:
-    task_services = TaskServices(TaskRepository(db))
+    task_services: TaskServices = TaskServices(TaskRepository(db))
     return task_services.create(task)
 
 @router.put("/{task_id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
 def update_task(task_id: uuid.UUID, task: TaskUpdate, db: Session = Depends(_db.get_db)) -> TaskResponse:
-    task_services = TaskServices(TaskRepository(db))
+    task_services: TaskServices = TaskServices(TaskRepository(db))
     return task_services.update(task_id, task)
 
 @router.delete("/{task_id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
 def delete_task(task_id: uuid.UUID, db: Session = Depends(_db.get_db)) -> None:
-    task_services = TaskServices(TaskRepository(db))
+    task_services: TaskServices = TaskServices(TaskRepository(db))
     task_services.delete(task_id)
