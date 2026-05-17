ee6bf1f9f8318580136b34a977686a039f7c2a41
MriteshAdak <mriteshadak@outlook.com>
2026-05-17 23:04:11 +0200

    Add default UUID generation for task ID in Task model

--- tasks_router/models/task_model.py
+++ tasks_router/models/task_model.py
@@ -13,7 +13,7 @@ from tasks_router.database.initiate_db import Base
 class Task(Base):
     __tablename__ = 'task'
     
-    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID, default=uuid.uuid4, primary_key=True)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey('user.id'), index=True, nullable=False)
     title: Mapped[str] = mapped_column(String, nullable=False)
     status: Mapped[str] = mapped_column(String, default='todo', nullable=False)
