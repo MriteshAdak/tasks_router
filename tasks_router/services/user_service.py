5af52e22dc7cf6e94485098a2ae4f48d11e7f6b5
MriteshAdak <mriteshadak@outlook.com>
2026-05-18 17:42:40 +0200

    Refactor UserService to use UserDTO and improve error handling in get_user and create methods

--- tasks_router/services/user_service.py
+++ tasks_router/services/user_service.py
@@ -3,7 +3,8 @@ UserService class that provides business logic for user management operations.
 This module defines the UserService class, which interacts with the UserRepository to perform CRUD operations on User entities. The UserService class also handles the conversion of UserModel instances to UserSchemas for API responses.
 """
 
-from tasks_router.schema.user_schema import User as UserSchema
+from tasks_router.exceptions.custom_exceptions import ServiceException, UserNotFoundException, DatabaseOperationException
+from tasks_router.schema.user_schema import User as UserDTO
 from tasks_router.models.user_model import User as UserModel
 from tasks_router.repositories.user_repo import UserRepository
 from tasks_router.utils import convert_user_model_to_user_schema_dto
@@ -14,18 +15,27 @@ class UserService:
         
         self.repository = repository
 
-    def get_user(self, username: str) -> UserSchema | None:
+    def get_user(self, username: str) -> UserDTO:
         """Service for retrieving a user by their username."""
 
-        user = self.repository.get_by_id(username)
-        return convert_user_model_to_user_schema_dto(user) if user else None
+        try:
+            user = self.repository.get_by_id(username)
+            return convert_user_model_to_user_schema_dto(user)
+        except UserNotFoundException:
+            raise
+        except Exception as e:
+            raise ServiceException(f"Error retrieving user with username {username}: {str(e)}") from e
 
-    def create(self, user: UserSchema) -> UserSchema:
+    def create(self, user: UserDTO) -> UserDTO:
         """Service for creating a new user in the database."""
 
-        new_user = UserModel(
-            username=user.username,
-            display_name=user.display_name,
-        )
-        created_user = self.repository.create(new_user)
-        return convert_user_model_to_user_schema_dto(created_user)
+        new_user_data = user.model_dump(exclude_unset=True)
+        new_user = UserModel(**new_user_data)
+
+        try:
+            created_user = self.repository.create(new_user)
+            return convert_user_model_to_user_schema_dto(created_user)
+        except DatabaseOperationException:
+            raise
+        except Exception as e:
+            raise ServiceException(f"Error creating user with username {user.username}: {str(e)}") from e
