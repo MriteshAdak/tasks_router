33302946373f71f0bd80b2bd8e608925c2439f49
MriteshAdak <mriteshadak@outlook.com>
2026-05-19 20:41:51 +0200

    Refactor FastAPI application initialization to use a lifespan function for database setup and teardown

--- tasks_router/main.py
+++ tasks_router/main.py
@@ -1,16 +1,29 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from tasks_router.routers.task_router import router as task_router
 from tasks_router.routers.user_router import router as user_router
 from tasks_router.routers.system_router import router as system_router
-from tasks_router.database.initiate_db import Database, Base
-from tasks_router.database.config_db import settings
+from tasks_router.database.initiate_db import Base
+from tasks_router.dependencies import db
 
-db: Database = Database(settings)
-Base.metadata.create_all(bind=db.get_engine())
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan function to handle application startup and shutdown events. It creates the database tables on startup."""
+    engine = db.get_engine()
+    Base.metadata.create_all(engine)
+    yield
+    engine.dispose()
 
-router = FastAPI(redirect_slashes=False)
+app = FastAPI(
+    title="Tasks Router API",
+    description="API for managing tasks and users with PostgreSQL database integration.",
+    version="1.0.0",
+    redirect_slashes=False,
+    lifespan=lifespan
+)
 
-router.include_router(task_router)
-router.include_router(user_router)
-router.include_router(system_router)
+app.include_router(task_router)
+app.include_router(user_router)
+app.include_router(system_router)
