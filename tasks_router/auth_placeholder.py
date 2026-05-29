51236526de5e2d3a2a9f850028d6c1b00a9308d9
MriteshAdak <mriteshadak@outlook.com>
2026-05-29 08:34:50 +0200

    Refactor get_current_user_id to return existing MOCK_USER_ID before assignment

--- tasks_router/auth_placeholder.py
+++ tasks_router/auth_placeholder.py
@@ -9,6 +9,10 @@ async def get_current_user_id() -> uuid.UUID:
     Placeholder function to simulate user authentication and retrieval of the current user's ID.
     """
     global MOCK_USER_ID
+    
+    if MOCK_USER_ID is not None:
+        return MOCK_USER_ID
+    
     MOCK_USER_ID = get_user_repository().get_all().first().id
 
     return MOCK_USER_ID
