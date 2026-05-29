bb3d81bbff5eb5582269afb7ccafb128f85b7541
MriteshAdak <mriteshadak@outlook.com>
2026-05-29 08:33:34 +0200

    Refactor MOCK_USER_ID initialization to occur within the get_current_user_id function

--- tasks_router/auth_placeholder.py
+++ tasks_router/auth_placeholder.py
@@ -2,11 +2,13 @@ import uuid
 
 from .dependencies import get_user_repository
 
-MOCK_USER_ID: uuid.UUID = get_user_repository().get_all().first().id
+MOCK_USER_ID: uuid.UUID
 
 async def get_current_user_id() -> uuid.UUID:
     """
     Placeholder function to simulate user authentication and retrieval of the current user's ID.
     """
+    global MOCK_USER_ID
+    MOCK_USER_ID = get_user_repository().get_all().first().id
 
     return MOCK_USER_ID
